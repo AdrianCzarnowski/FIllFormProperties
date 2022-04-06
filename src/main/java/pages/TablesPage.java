fd0a0bfa5b9695ff1f31e8d9071b57d71eb16307
aczarnowski <adriannnczarnowski@gmail.com>
2022-04-06 14:09:52 +0200

    with Tables

--- src/main/java/pages/TablesPage.java
+++ src/main/java/pages/TablesPage.java
@@ -1,4 +1,45 @@
 package pages;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.List;
+
 public class TablesPage {
+    private static Logger log = LoggerFactory.getLogger("TablesPage.class");
+
+    @FindBy(css = "tbody tr")
+    private List<WebElement> rows;
+//    @FindBy(xpath = "./td[4]")
+//    WebElement height;
+//    @FindBy(xpath = "./td[3]")
+//    WebElement state;
+//    @FindBy(xpath = "td")
+//    WebElement rank;
+
+    public TablesPage(WebDriver driver) {
+        PageFactory.initElements(driver, this);
+    }
+
+    public TablesPage pickMountain() {
+        for (WebElement row : rows) {
+
+            int height = Integer.parseInt(row.findElement(By.xpath("./td[4]")).getText());
+            String state = row.findElement(By.xpath("./td[3]")).getText();
+
+            if (height >4000 && state.contains("Switzerland")) {
+                List<WebElement> cols = row.findElements(By.cssSelector("td"));
+                List<WebElement> cols1 = row.findElements(By.cssSelector("th"));
+                List<WebElement> range = row.findElements(By.cssSelector("td:nth-child(3)"));
+                log.info("Rank: " + cols1.get(0).getText() + " <<Peak: " + cols.get(0).getText() + " <<Mountain rage: " + range.get(0).getText());
+            }
+        }
+        return this;
+    }
 }
+
--- src/test/java/TablesTest.java
+++ src/test/java/TablesTest.java
@@ -1,10 +1,9 @@
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import pages.TablesPage;
 
-import java.util.List;
+
 
 
 public class TablesTest extends TestBase {
@@ -13,21 +12,8 @@ public class TablesTest extends TestBase {
     @Test
     public void mountainsInSwitzerland() {
         driver.get("https://seleniumui.moderntester.pl/table.php");
-
-        List<WebElement> rows = driver.findElements(By.cssSelector("tbody tr"));
-
-        for (WebElement row : rows){
-
-            int height = Integer.parseInt(row.findElement(By.xpath("./td[4]")).getText());
-            String state = row.findElement(By.xpath("./td[3]")).getText();
-
-            if (height >4000 && state.contains("Switzerland")) {
-                List<WebElement> cols = row.findElements(By.cssSelector("td"));
-                List<WebElement> cols1 = row.findElements(By.cssSelector("th"));
-                List<WebElement> range = row.findElements(By.cssSelector("td:nth-child(3)"));
-                log.info("Rank: " + cols1.get(0).getText() + " <<Peak: " + cols.get(0).getText() + " <<Mountain rage: " + range.get(0).getText());
-            }
-
+        TablesPage tablesPage = new TablesPage(driver);
+                   tablesPage.pickMountain();
         }
     }
-}
+
